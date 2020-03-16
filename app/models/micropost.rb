cd96342b485747059ea7535be37e9345fc33b24d
kazumasa.tomoto <kazurate@gmail.com>
2020-03-16 07:04:46 +0000

    お気に入り機能

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,5 +1,11 @@
 class Micropost < ApplicationRecord
   belongs_to :user
   
+  has_many :favorites
+  has_many :add_to_favorites, through: :fovorites, source: :user
+  
+  validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 255 }
+  
+
 end
